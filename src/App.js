fb242bb92ee3fb4ad7c4c536eb8488e6b1011c18
Thomas England <tengland808@gmail.com>
2022-03-05 09:53:39 -1000

    update our menu-items with useNavigate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
+
+const HatsPage = () => (
+  <div>
+    <h3> HATS PAGE</h3>
+    <h4>h4 is not a real html tag is it. not sure. </h4>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
